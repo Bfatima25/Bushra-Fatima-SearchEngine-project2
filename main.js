dd21dfd14a9dc29fbeab28180b31497b346816cc
Bfatima25 <bushrafatima102@gmail.com>
2021-12-02 19:42:14 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function sendApi() {
+    var userInput =
+document.getElementById('searchText').value
+console.log(userInput);
+
+var giphyApiKey = "XtHkRU31WHHyqyBBBkLrmJgQdtkGIoZD"
+var LIMIT = 25;
+var giphyApiURL = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${userInput}&limit=${LIMIT}&offset=0&rating=g&lang=en`
+
+fetch(giphyApiURL).then(function(data){
+    return data.json()
+})
+.then(function(json){
+  
+  console.log(json.data[0].images.fixed_height.url)
+    var imgPath = json.data[0].images.fixed_height.url
+    var img = document.createElement("img");
+    img.setAttribute("src", imgPath);
+    img.setAttribute("class", "giphy-container");
+    document.body.appendChild(img);
+})
+}
+
+
+
+
