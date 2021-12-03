7edf32aabba18c324a9ae04ca60d80e953b84590
Bfatima25 <bushrafatima102@gmail.com>
2021-12-03 15:47:27 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+document.getElementById('btn-search').addEventListener("click", ev => {
+    ev.preventDefault();
+});
 function sendApi() {
     var userInput =
 document.getElementById('searchText').value
@@ -11,13 +14,15 @@ fetch(giphyApiURL).then(function(data){
     return data.json()
 })
 .then(function(json){
-  
   console.log(json.data[0].images.fixed_height.url)
     var imgPath = json.data[0].images.fixed_height.url
     var img = document.createElement("img");
     img.setAttribute("src", imgPath);
     img.setAttribute("class", "giphy-container");
     document.body.appendChild(img);
+})
+.catch(err => {
+    console.log("Not Found")
 })
 }
 
